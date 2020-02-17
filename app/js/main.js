942ad8a26b589f2f1a92b0393707563f2b8fce95
Vlada-kr <56648740+Vlada-kr@users.noreply.github.com>
2020-02-17 16:31:26 +0300

    Added section about

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.header__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -9,7 +9,29 @@ $(function(){
         autoplaySpeed: 2000
     });
 
-    $('.header__menu-text').on('click',function(){
+    $('.header__menu-text').on('click', function () {
         $('.header__menu-list').slideToggle();
-    })
+    });
+
+
+    $('.about__slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.about__slider-nav'
+    });
+    $('.about__slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.about__slider-for',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
 });
